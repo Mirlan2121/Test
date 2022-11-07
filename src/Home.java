2d456b4d5a6cd9319eb0ecbc71e770b1907d3506
Эрик уулу Мирлан <m_erik@cbk.kg>
2022-11-07 19:42:14 +0600

    Initial commit

---
+++ src/Home.java
@@ -0,0 +1,9 @@
+public class Home {
+    private int ligte;
+    private int higte;
+    private int shirt;
+    private String address;
+    private int number;
+
+
+}
---
+++ src/Room.java
@@ -0,0 +1,43 @@
+
+public class Room {
+    private int light;
+    private int height;
+    private int door;
+
+
+
+    public int getLight() {
+        return light;
+    }
+
+    public void setLight(int light) {
+        this.light = light;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getDoor() {
+        return door;
+    }
+
+    public void setDoor(int door) {
+        this.door = door;
+    }
+
+    public int getWindows() {
+        return windows;
+    }
+
+    public void setWindows(int windows) {
+        this.windows = windows;
+    }
+
+    private int windows;
+
+}
